2939c71885cfb269155be98206aa3bee04146264
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-27 23:48:15 -0300

    excluindo import não utilizado nos testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
